002323d11c39b6b8899e6d94d211f59c5c28ad85
mp <mp@maya.cz>
2017-09-13 17:58:18 +0200

    removed unnecesarry defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace 'api', defaults: { format: :json } do
+  namespace 'api' do
     namespace 'v1' do
       post 'klik', to: 'kliks#klik'
       get 'leaderboard', to: 'kliks#leaderboard'
